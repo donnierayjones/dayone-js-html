30d858e8eec96f3b63f77131d10f2f9df27db4e6
donnierayjones <donnierayjones@gmail.com>
2015-03-24 20:51:19 -0500

    first working prototype

--- script.js
+++ script.js
@@ -1,65 +1,189 @@
 $(function() {
   var fileSelector = '#fileSelector';
   var dragAndDropSelector = '#dragAndDrop';
-  var fileInputSelector = '#fileInput';
-  var renderTarget = '#dayOneRenderTarget';
+  var renderTargetSelector = '#dayOneRenderTarget';
+  var entryTemplateSelector = '#dayOneEntryTemplate';
+
+  var DayOneEntry = function(plistData) {
+    this.data = plistData;
+    this.photoDataURL = '';
+  };
+
+  DayOneEntry.prototype.get = function(key) {
+    return this.data[key];
+  };
+
+  DayOneEntry.prototype.UUID = function() {
+    return this.get('UUID');
+  };
+
+  DayOneEntry.prototype.creationDateNumeric = function() {
+    return new Date(this.get('Creation Date')).getTime();
+  };
+
+  DayOneEntry.prototype.creationDateTime = function() {
+    return moment(this.get('Creation Date')).format('MMMM Do YYYY, h:mm a');
+  };
+
+  DayOneEntry.prototype.creationDate = function() {
+    return moment(this.get('Creation Date')).format('MMMM Do YYYY');
+  };
+
+  DayOneEntry.prototype.creationTime = function() {
+    return moment(this.get('Creation Date')).format('h:mm a');
+  };
+
+  DayOneEntry.prototype.text = function() {
+    return marked(this.get('Entry Text'));
+  };
+
+  DayOneEntry.prototype.hasPhoto = function() {
+    return this.photoDataURL != '';
+  };
+
+  DayOneEntry.prototype.setPhotoDataURL = function(dataURL) {
+    this.photoDataURL = dataURL;
+  };
+
+  DayOneEntry.prototype.getPhotoDataURL = function() {
+    return this.photoDataURL;
+  };
 
   var DayOneRenderer = function() {
     var _this = this;
-    this.loadImageCounter = 0;
+    this.entries = {};
+
     $(dragAndDropSelector).on('drop', function(e) {
       e.stopPropagation();
       e.preventDefault();
-      _this.handleFileSelect(e.originalEvent.dataTransfer.files);
+      _this.handleFileSelect(e.originalEvent.dataTransfer.items);
     }).on('dragover dragenter', function(e) {
       e.preventDefault();
       e.stopPropagation();
     });
+  };
 
-    $(fileInputSelector).on('change', function(e) {
-      _this.handleFileSelect(e.target.files);
+  DayOneRenderer.prototype.onEntryTextLoaded = function(text) {
+    var plistData = PlistParser.parse(text);
+    this.addEntry(new DayOneEntry(plistData));
+  };
+
+  DayOneRenderer.prototype.addEntry = function(dayOneEntry) {
+    this.entries[dayOneEntry.UUID()] = dayOneEntry;
+  };
+
+  DayOneRenderer.prototype.getEntries = function() {
+    return _.sortBy(_.values(this.entries), function(e) {
+      return e.creationDateNumeric();
     });
   };
 
   DayOneRenderer.prototype.handleFileSelect = function(files)  {
     var _this = this;
-    // use a BlobReader to read the zip from a Blob object
-    zip.createReader(new zip.BlobReader(files[0]), function(reader) {
 
-      // get all entries from the zip
-      reader.getEntries(function(entries) {
-        for(var i = 0; i < entries.length; i++) {
-          var entry = entries[i];
-          if(entry.filename.match(/.*\.doentry/)) {
-            // get first entry content as text
-            entry.getData(new zip.TextWriter(), function(text) {
-              // text contains the entry data as a String
-              var jsonString = PlistParser.parse(text);
-              console.log(jsonString);
-            });
+    if(files.length === 0) {
+      return;
+    };
+    var root = files[0].webkitGetAsEntry();
+
+    this.loadEntries(root, function() {
+      _this.render();
+    });
+  };
+
+  DayOneRenderer.prototype.loadEntries = function(root, onComplete) {
+    var _this = this;
+    root.getDirectory('entries', null, function(entriesDirectory) {
+      var directoryReader = entriesDirectory.createReader();
+      var allEntries = [];
+      var processEntries = function() {
+        var entryCount = allEntries.length;
+        var entriesProcessed = 0;
+        allEntries.forEach(function(entry) {
+          entry.file(function(file) {
+            var fileReader = new FileReader();
+            fileReader.onload = function() {
+              _this.onEntryTextLoaded(fileReader.result);
+              entriesProcessed++;
+              if(entriesProcessed == entryCount) {
+                _this.loadPhotos(root, onComplete);
+              }
+            };
+            fileReader.readAsText(file, 'UTF-8');
+          });
+        });
+      };
+      var readEntries = function() {
+        directoryReader.readEntries(function(entries) {
+          if(entries.length == 0) {
+            processEntries(allEntries);
           }
-          _this.loadImageCounter++;
-          if(entry.filename.match(/.*\.jpg/) && _this.loadImageCounter <= 10) {
-            entry.getData(new zip.BlobWriter(), function(blob) {
-              loadImage(
-                  blob,
-                  function (img) {
-                    if(img.type === "error") {
-                      console.log("Error loading image ", entry);
-                    } else {
-                      document.body.appendChild(img);
-                    }
-                  }, {
-                    maxWidth: 600
-                  }
-              );
-            });
+          else
+          {
+            allEntries = allEntries.concat(entries);
+            readEntries();
           }
-        }
-      });
-    }, function(error) {
-      // onerror callback
-      console.log(error);
+        });
+      };
+      readEntries();
+    });
+  };
+
+  DayOneRenderer.prototype.loadPhotos = function(root, onComplete) {
+    var _this = this;
+    root.getDirectory('photos', null, function(photosDirectory) {
+      var directoryReader = photosDirectory.createReader();
+      var allPhotos = [];
+      var processPhotos = function(photos) {
+        var photosProcessed = 0;
+        photos.forEach(function(photo) {
+          photo.file(function(file) {
+            var fileName = file.name.substring(0, 32);
+            var fileReader = new FileReader();
+            fileReader.onload = function(e) {
+                var dataURL = fileReader.result;
+                var entry = _this.entries[fileName];
+                if(entry) {
+                  entry.setPhotoDataURL(dataURL);
+                } else {
+                  console.log("No entry found for photo: " + fileName);
+                }
+                photosProcessed++;
+                if(photosProcessed === photos.length) {
+                  onComplete();
+                }
+            }
+            fileReader.readAsDataURL(file);
+          });
+        });
+      };
+      var readPhotos = function() {
+        directoryReader.readEntries(function(photos) {
+          if(photos.length === 0) {
+            processPhotos(allPhotos);
+          }
+          else {
+            allPhotos = allPhotos.concat(photos);
+            readPhotos();
+          }
+        });
+      };
+      readPhotos();
+    });
+  };
+
+  DayOneRenderer.prototype.render = function() {
+    var $target = $(renderTargetSelector);
+    $(fileSelector).hide();
+    $target.show();
+
+    var template = _.template($(entryTemplateSelector).html());
+
+    this.getEntries().forEach(function(entry) {
+      setTimeout(function() {
+        var html = template({entry: entry});
+        $target.append(html);
+      }, 0);
     });
   };
 
